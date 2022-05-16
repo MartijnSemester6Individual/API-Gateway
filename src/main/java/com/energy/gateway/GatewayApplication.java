8ee9d0a48b86c911a2cf0fb0ab337d2e466a5abb
MartijnBassa <martijnbassa2009@hotmail.com>
2022-05-16 14:02:18 +0200

    Update ports

--- src/main/java/com/energy/gateway/GatewayApplication.java
+++ src/main/java/com/energy/gateway/GatewayApplication.java
@@ -1,6 +1,5 @@
 package com.energy.gateway;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,11 +9,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-            .directory(".env")
-            .ignoreIfMalformed()
-            .ignoreIfMissing()
-            .load();
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
